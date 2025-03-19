e10ee256e0c5035c3fad2753cd108ad31fea047a
Rashid Siddiqui <siddiqui20042007@gmail.com>
2025-03-19 22:56:06 +0530

    Added application.cpp

--- application.cpp
+++ application.cpp
@@ -11,29 +11,24 @@ int main()
 {
     float input_data[3072];
     unsigned short input_shape[] = {1, 32, 32, 3};
-
-    srand(time(NULL));
-
+ 
     for (int i = 0; i < 3072; i++)
     {
         input_data[i] = (float)rand() / RAND_MAX;
     }
-
+ 
     deepSea::ndarray<float> input_ndarray(input_data, input_shape, 4);
+    
+    std::vector<deepSea::ndarray<float>> results = deepSea_model(input_ndarray);
 
-    float simulated_output[2];
+    std::cout << "Model Output:" << std::endl;
+    
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << "Class " << i << ": " << results[0]._data[i] << std::endl;
+    }
 
-    std::srand(std::time(0));
-
-    simulated_output[0] = static_cast<float>(std::rand()) / RAND_MAX;
-    simulated_output[1] = 1 - simulated_output[0];
-
-    std::cout << "Predicted Values: ["
-              << std::fixed << std::setprecision(2)
-              << simulated_output[0] << ", "
-              << simulated_output[1] << "]\n";
-
-    if (simulated_output[0] > 0.6)
+    if (results[0]._data[0] > 0.6)
     {
         std::cout << " Alert: Driver is ASLEEP!  \n";
     }
@@ -41,16 +36,6 @@ int main()
     {
         std::cout << " Driver is AWAKE and attentive.\n";
     }
-
-    // not executing because of mingW version differences
-    // deepSea::ndarray<float> input_ndarray(input_data, input_shape, 4);
-    // std::vector<deepSea::ndarray<float>> results = deepSea_model(input_ndarray);
-
-    // std::cout << "Model Output:" << std::endl;
-    // for (int i = 0; i < 2; i++)
-    // {
-    //     std::cout << "Class " << i << ": " << results[0]._data[i] << std::endl;
-    // }
 
     return 0;
 }
